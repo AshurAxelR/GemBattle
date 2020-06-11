e9a31ba889205d7b0b6f7fa5d519af67cfc136d4
AshurAxelR <7903175+AshurAxelR@users.noreply.github.com>
2020-06-11 20:46:25 +0100

    Particles

--- app/src/main/java/com/xrbpowered/android/gembattle/MainActivity.java
+++ app/src/main/java/com/xrbpowered/android/gembattle/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 
 	public static final int frameTime = 25;
 
-	private BaseView base = null;
+	private RepaintHandler repaint;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +23,16 @@ public class MainActivity extends Activity {
 		GemBattle.boldFont = ResourcesCompat.getFont(this, R.font.montserrat_black);
 
 		setContentView(R.layout.activity_main);
-		base = findViewById(R.id.baseview);
+		BaseView base = findViewById(R.id.baseview);
 		GemBattle.gamePane = new GamePane(new UIFitScaleContainer(base.getContainer(), GamePane.targetWidth, GamePane.targetHeight));
 
-		GemBattle.repaint = new RepaintHandler(base, frameTime);
+		repaint = new RepaintHandler(base, frameTime);
 	}
 
 	@Override
 	protected void onPause() {
-		if(base!=null) {
-			GemBattle.repaint.setFrameTime(0);
+		if(repaint!=null) {
+			repaint.setFrameTime(0);
 		}
 		super.onPause();
 	}
@@ -40,8 +40,8 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		if(base!=null) {
-			GemBattle.repaint.setFrameTime(frameTime);
+		if(repaint!=null) {
+			repaint.setFrameTime(frameTime);
 		}
 	}
 }
---
+++ app/src/main/java/com/xrbpowered/android/gembattle/effects/particles/DotParticle.java
@@ -0,0 +1,35 @@
+package com.xrbpowered.android.gembattle.effects.particles;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+
+import com.xrbpowered.android.gembattle.ui.RenderUtils;
+
+public class DotParticle extends Particle {
+
+	private static final Paint dotPaint = new Paint();
+	static {
+		dotPaint.setStyle(Paint.Style.FILL);
+		dotPaint.setAntiAlias(false);
+	}
+
+	private final ColorTween color;
+	private final float r0, r1;
+
+	public DotParticle(float x0, float y0, float x1, float y1, float r0, float r1, float duration, ColorTween color) {
+		this.sourcePoint = new PointF(x0, y0);
+		this.targetPoint = new PointF(x1, y1);
+		this.duration = duration;
+		this.color = color;
+		this.r0 = r0;
+		this.r1 = r1;
+	}
+
+	@Override
+	protected void draw(Canvas canvas, float x, float y, float ts, Paint paint) {
+		dotPaint.setColor(color.get(ts));
+		float r = RenderUtils.lerp(r0, r1, ts);
+		canvas.drawCircle(x, y, r, dotPaint);
+	}
+}
